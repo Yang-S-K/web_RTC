f0fb91360890800e6b8c50a784ed7358191ac5ef
YskEDF <64305492+Yang-S-K@users.noreply.github.com>
2025-10-06 15:16:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getDatabase, ref, set, get, remove } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
+  apiKey: "AIzaSyAg9yuhB3c5s4JqQ_sW7iTVAr3faI3pdd8",
+  authDomain: "web-rtc-1f615.firebaseapp.com",
+  databaseURL: "https://web-rtc-1f615-default-rtdb.asia-southeast1.firebasedatabase.app", // 🔥 改這裡
+  projectId: "web-rtc-1f615",
+  storageBucket: "web-rtc-1f615.appspot.com",
+  messagingSenderId: "369978320587",
+  appId: "1:369978320587:web:8f1bf80a69c19e21051f4e"
   databaseURL: "https://web-rtc-1f615-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
 const app = initializeApp(firebaseConfig);
