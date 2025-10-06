904092692d27f738961364e1544c00a8e8acb81f
YskEDF <64305492+Yang-S-K@users.noreply.github.com>
2025-10-06 15:18:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: "web-rtc-1f615",
   storageBucket: "web-rtc-1f615.appspot.com",
   messagingSenderId: "369978320587",
-  appId: "1:369978320587:web:8f1bf80a69c19e21051f4e"
+  appId: "1:369978320587:web:8f1bf80a69c19e21051f4e",
   databaseURL: "https://web-rtc-1f615-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
 const app = initializeApp(firebaseConfig);
