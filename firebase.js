166d93c07ce2a5733d257f09de4dbcd7cc064e63
YskEDF <64305492+Yang-S-K@users.noreply.github.com>
2025-10-08 22:40:58 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+// 你的 Firebase 設定
+const firebaseConfig = {
+  apiKey: "AIzaSyAg9yuhB3c5s4JqQ_sW7iTVAr3faI3pdd8",
+  authDomain: "web-rtc-1f615.firebaseapp.com",
+  databaseURL: "https://web-rtc-1f615-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "web-rtc-1f615",
+  storageBucket: "web-rtc-1f615.appspot.com",
+  messagingSenderId: "369978320587",
+  appId: "1:369978320587:web:8f1bf80a69c19e21051f4e"
+};
+
+// 初始化 Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
