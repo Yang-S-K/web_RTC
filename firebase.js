b472bba1ae70bbedceda141131d50698c6a3d5c5
YskEDF <64305492+Yang-S-K@users.noreply.github.com>
2025-10-09 15:39:11 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,6 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { getDatabase, ref, set, get, remove, onValue, update, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-// 你的 Firebase 設定
 const firebaseConfig = {
   apiKey: "AIzaSyAg9yuhB3c5s4JqQ_sW7iTVAr3faI3pdd8",
   authDomain: "web-rtc-1f615.firebaseapp.com",
@@ -13,6 +11,7 @@ const firebaseConfig = {
   appId: "1:369978320587:web:8f1bf80a69c19e21051f4e"
 };
 
-// 初始化 Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+// Re-export database functions for convenience
+export { ref, set, get, remove, onValue, update, serverTimestamp };
