84ccef2fb432cb1e430f58c8019457e89533d298
YskEDF <64305492+Yang-S-K@users.noreply.github.com>
2025-10-12 00:53:51 +0800

    Update webrtc.js

--- webrtc.js
+++ webrtc.js
@@ -7,6 +7,7 @@ const configuration = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
   ],
+  {
       urls: [
         'turn:global.turn.xirsys.net:3478?transport=udp',
         'turn:global.turn.xirsys.net:3478?transport=tcp',
